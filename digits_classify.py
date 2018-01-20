a831265eed7f012d282dd884fbe6e135ea2b86e3
Dawid Sielski <dawid.sielski@outlook.com>
2018-01-20 21:46:53 +0100

    Simple recognition

--- digits_classify.py
+++ digits_classify.py
@@ -2,3 +2,15 @@ from sklearn import datasets, svm, metrics, externals
 
 digits = datasets.load_digits()
 
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+
+classifier = svm.SVC(gamma=0.001)
+
+classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
+
+expected = digits.target[n_samples // 2:]
+predicted = classifier.predict(data[n_samples // 2:])
+
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
